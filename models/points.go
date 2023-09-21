10f8ee0e27fb31cac2938e232cb81c2c67a62023
goku <hjbello@protonmail.com>
2023-09-21 21:19:25 +0200

    clean

--- models/points.go
+++ models/points.go
@@ -3,10 +3,11 @@ package models
 import "time"
 
 type PointsReport struct {
-	Week      int        `json:"week" bson:"week"`
-	Points    int        `json:"points" bson:"points"`
-	CreatedAt *time.Time `json:"editedAt" bson:"editedAt"`
-	BoardId   string     `json:"boardId" bson:"boardId"`
+	Week               int           `json:"week" bson:"week"`
+	Points             int           `json:"points" bson:"points"`
+	TasksSummingPoints []TaskHistory `json:"tasksSummingPoints" bson:"tasksSummingPoints"`
+	CreatedAt          *time.Time    `json:"editedAt" bson:"editedAt"`
+	BoardId            string        `json:"boardId" bson:"boardId"`
 }
 
 type PointsReportResponse struct {
